e66dc8af6e17b58270dcb99a81e447356f64ba17
Iqoqacola <iqoqacola@gmail.com>
2024-05-25 16:48:22 +0800

    testing

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -10,11 +10,14 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/api/user/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(
+      "https://restapi-taskmanager-production.up.railway.app/api/user/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
